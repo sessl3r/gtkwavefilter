941741f6fe64fdca29c914e9bd7e1e3a7e1972ff
Tobias Binkowski <tobias.binkowski@missinglinkelectronics.com>
2024-09-23 10:12:11 +0200

    transaction: change template for issue

--- transaction/template.py
+++ transaction/template.py
@@ -1,46 +1,54 @@
 #!/usr/bin/env python3
 
 import sys
-from wave2data.input import VCDWaveInput
 
 
 # Monkey patch _TokenizerState.advance to be able to use streams
-def __monkey_advance(self, raise_on_eof: bool = True) -> int:
-    if self.pos < self.end:
-        self.pos += 1
-    else:
-        self.buf = self.stream.readline()
-        n = len(self.buf)
-        if n:
-            self.end = n - 1
-            self.pos = 0
-        elif raise_on_eof:
-            raise StopIteration()
-        else:
-            return 0
-    c = self.buf[self.pos]
-    if c == 10:
-        self.lineno += 1
-        self.column = 1
-    else:
-        self.column += 1
-    return self.buf[self.pos]
-
-
-vcd.reader._TokenizerState.advance = __monkey_advance
+# def __monkey_advance(self, raise_on_eof: bool = True) -> int:
+#     if self.pos < self.end:
+#         self.pos += 1
+#     else:
+#         self.buf = self.stream.readline()
+#         n = len(self.buf)
+#         if n:
+#             self.end = n - 1
+#             self.pos = 0
+#         elif raise_on_eof:
+#             raise StopIteration()
+#         else:
+#             return 0
+#     c = self.buf[self.pos]
+#     if c == 10:
+#         self.lineno += 1
+#         self.column = 1
+#     else:
+#         self.column += 1
+#     return self.buf[self.pos]
+#
+#
+# vcd.reader._TokenizerState.advance = __monkey_advance
 
 
 def main():
-    wave = VCDWaveInput(sys.stdin.fileno())
+#    wave = VCDWaveInput(sys.stdin.fileno())
+#    while True:
+#        for sample in wave:
+#            if not isinstance(sample, vcd.reader.Token):
+#                continue
+#            # handle gtkwave's data_end
+#            if sample.kind == vcd.reader.TokenKind.COMMENT:
+#                if "data_end" in sample.data:
+#                    break
+#            print(sample, file=sys.stderr)  # TODO: debugging, remove in real code
+
     while True:
-        for sample in wave:
-            if not isinstance(sample, vcd.reader.Token):
-                continue
-            # handle gtkwave's data_end
-            if sample.kind == vcd.reader.TokenKind.COMMENT:
-                if "data_end" in sample.data:
-                    break
-            print(sample, file=sys.stderr)  # TODO: debugging, remove in real code
+        while True:
+            line = sys.stdin.readline()
+            print(line, end="", file=sys.stderr)
+            if not line:
+                return
+            if "data_end" in line:
+                break
 
         print("$name Val1")
         print("#0 Val1:Test1?")
